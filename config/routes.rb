efcbe4e35f6629d266a3ede3db735ebb5902629d
Elana Kopelevich <ekopelevich@gmail.com>
2018-11-22 13:22:56 -0700

    version api and add sample-env

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :items do
-    resources :tags
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :items do
+      resources :tags
+    end
   end
+
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
