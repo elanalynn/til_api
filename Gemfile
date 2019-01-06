3f54a4b5d23661aa56dd667b7cf6f656f5cc73b9
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-05 20:57:45 -0700

    install pry and add wrap_parameters format: [] to auth controller

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ end
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry'
   gem 'dotenv-rails'
   gem 'rspec-rails', '~> 3.5'
   gem 'sqlite3'
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,6 @@
 class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
+  wrap_parameters format: []
 
   def authenticate
     auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
