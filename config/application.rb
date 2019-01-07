915062733f2e65ca297e73e3b94a92bc1496aaad
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-06 20:26:03 -0700

    adding a slash../.. I dunno

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module TilApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://todayilearned-client.herokuapp.com'
+        origins 'https://todayilearned-client.herokuapp.com/'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
