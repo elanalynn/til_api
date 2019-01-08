1c57c9f08cdbf93423db23c498b2e313ac670bc4
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-07 20:04:17 -0700

    try skipping csrf check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::API
   include ExceptionHandler
 
   before_action :authorize_request
+  skip_before_filter :verify_authenticity_token
+
   attr_reader :current_user
 
   private
